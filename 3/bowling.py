4e5a882f26cf91cfb39e7cd6d9588a8fff9cacec
okkdev <dev@stehlik.me>
2020-12-09 10:17:31 +0100

    🔥 simplify

--- 3/bowling.py
+++ 3/bowling.py
@@ -11,10 +11,6 @@ for i, _ in enumerate(range(0, spieler), start=1):
         scores.append(int(input("Wert: ")))
     resultate.append(scores)
 
-summe = {}
-for i, resultat in enumerate(resultate, start=1):
-    summe["Spieler " + str(i)] = sum(resultat)
-
 print("--------RESULTATE--------")
-for s in summe:
-    print(s + " Punkte: " + str(summe[s]))
+for i, resultat in enumerate(resultate, start=1):
+    print("Spieler " + str(i) + " Punkte: " + str(sum(resultat)))
