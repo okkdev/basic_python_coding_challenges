1f45694e0e21489f5965cb156387225cd421f03e
okkdev <dev@stehlik.me>
2020-12-11 14:44:52 +0100

    🎨 lists

--- 3/bowling.py
+++ 3/bowling.py
@@ -4,13 +4,13 @@ runden = int(input("Anzahl Runden: "))
 resultate = []
 
 for i in range(1, spieler + 1):
-    print("Spieler " + str(i))
-    score = 0
+    print("Spieler ", i)
+    scores = []
     for i in range(1, runden + 1):
-        print("Runde " + str(i))
-        score += int(input("Wert: "))
-    resultate.append(score)
+        print("Runde ", i)
+        scores.append(int(input("Wert: ")))
+    resultate.append(scores)
 
 print("--------RESULTATE--------")
 for i, resultat in enumerate(resultate, start=1):
-    print("Spieler ", i, " Punkte: ", resultat)
+    print("Spieler ", i, " Punkte: ", sum(resultat))
